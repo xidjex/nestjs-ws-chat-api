171d22c880a9faf2015a5662343dfb82f51a38d5
dmitriy <xidjex@bk.ru>
2020-01-09 15:21:22 +0200

    Added admin user guard

---
+++ src/events/guards/admin.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ForbiddenException } from '../../auth/exceptions/forbidden.exception';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const client = context.switchToWs().getClient();
+
+    const user = client.handshake.query.user;
+
+    if (!user.isAdmin) {
+      throw new ForbiddenException('This action allowed only for an admin users.');
+    }
+
+    return true;
+  }
+}
